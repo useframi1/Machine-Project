7e06558a0481961bd7a47491ca228391774f7937
Youssef Rami <yousseframi@aucegypt.edu>
2024-03-29 21:37:38 +0200

    cleaned up a bit

--- MS3/binning.py
+++ MS3/binning.py
@@ -5,7 +5,7 @@ def bin(label: np.ndarray):
     label_copy = label.copy()
     label_copy[label <= 15] = 0
     label_copy[(label_copy > 15) & (label_copy <= 45)] = 1
-    label_copy[(label_copy > 45) & (label_copy <= 105)] = 2
-    label_copy[label_copy > 105] = 3
+    label_copy[(label_copy > 45) & (label_copy <= 120)] = 2
+    label_copy[label_copy > 120] = 3
 
     return label_copy
